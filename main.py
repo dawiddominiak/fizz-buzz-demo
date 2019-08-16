54768a103a4c86b133601df351f37297d1d0e7d6
Dawid Dominiak <dawiddominiak@MacBook-Pro-Dawid-2.local>
2019-08-16 16:26:34 +0200

    allow edit loop_length by argv

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from engine.fizz_buzz_loop import start_fizz_buzz_loop
+from sys import argv
 
-start_fizz_buzz_loop()
+loop_length = 100 # default
+
+if len(argv) >= 2:
+  loop_length = int(argv[1])
+
+start_fizz_buzz_loop(loop_length)
