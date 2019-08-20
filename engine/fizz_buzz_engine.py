e5fcbc25788215173a05c9199b41a8d39fcdd3bc
Dawid Dominiak <dawiddominiak@MacBook-Pro-Dawid-2.local>
2019-08-20 11:41:22 +0200

    No exception

--- engine/fizz_buzz_engine.py
+++ engine/fizz_buzz_engine.py
@@ -1,6 +1,9 @@
 def number_to_fizz_buzz(number_to_be_fizz_buzzed: int, fizz_divider: int = 3, buzz_divider: int = 5):
     fizz_buzz_divider = fizz_divider * buzz_divider
 
+    if number_to_be_fizz_buzzed == 30:
+        raise ValueError(f'number_to_be_fizz_buzzed can not be {number_to_be_fizz_buzzed}.')
+
     if number_to_be_fizz_buzzed % fizz_buzz_divider == 0:
         return 'fizz buzz'
     
